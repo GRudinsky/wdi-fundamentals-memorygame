3a6ead30b8bd49b5038cafb8678edb2ebc2a5a51
GRudinsky <38439393+GRudinsky@users.noreply.github.com>
2019-08-06 16:44:47 +0100

    Add files via upload
    Created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+
+
+var cards = ['queen', 'king', 'queen', 'king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length ===2){if (cardsInPlay[0] === cardsInPlay[1]) {alert('You found a match!')
+
+} else { alert('Sorry, try again');
+}};
+
+var cardThree = cards[2];
+cardsInPlay.push(cardThree);
+var cardFour = cards[3];
+cardsInPlay.push(cardFour);
 
 console.log("User flipped " + cardOne);
