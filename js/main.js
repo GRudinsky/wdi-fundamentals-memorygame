b38136d703e4a9e2471f92dbf3f37da2de91b689
GRudinsky <38439393+GRudinsky@users.noreply.github.com>
2019-08-11 10:10:58 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 
 
+
 var cards = ['queen', 'king', 'queen', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length ===2){if (cardsInPlay[0] === cardsInPlay[1]) {alert('You found a match!')
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert('You found a match!')
+  } else {
+    alert('Sorry, try again');
+  }
+};
+function flipCard(cardId) {
+  cardsInPlay.push(cards[cardId]);
+  console.log('User flipped '+ cards[cardId]);
+  checkForMatch();
+};
 
-} else { alert('Sorry, try again');
-}};
-
-var cardThree = cards[2];
-cardsInPlay.push(cardThree);
-var cardFour = cards[3];
-cardsInPlay.push(cardFour);
-
-console.log("User flipped " + cardOne);
+  flipCard(0);
+  flipCard(2);
