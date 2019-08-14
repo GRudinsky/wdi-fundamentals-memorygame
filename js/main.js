9e7573708cd7d327630bf81f250d1357fe5fb052
GRudinsky <38439393+GRudinsky@users.noreply.github.com>
2019-08-14 10:44:37 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 
 
 
-var cards = ['queen', 'king', 'queen', 'king'];
+// var cards = ['queen', 'king', 'queen', 'king'];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 var cardsInPlay = [];
 function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -12,9 +34,9 @@ function checkForMatch() {
 };
 function flipCard(cardId) {
   cardsInPlay.push(cards[cardId]);
-  console.log('User flipped '+ cards[cardId]);
+  console.log('User flipped '+ cards[cardId].rank + " of " + cards[cardId].suit);
   checkForMatch();
 };
 
   flipCard(0);
-  flipCard(2);
+  flipCard(0);
