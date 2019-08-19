e92ef4cdb99527c0a3c5a2a893d025e46e9509cf
GRudinsky <38439393+GRudinsky@users.noreply.github.com>
2019-08-19 17:30:17 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -24,19 +24,74 @@ var cards = [
     cardImage: "images/king-of-diamonds.png"
   }
 ];
+
 var cardsInPlay = [];
+var gamesWon = 0;
+var gamesLost = 0;
+
 function checkForMatch() {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert('You found a match!')
+  if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
+    alert('You found a match!');
+    gamesWon += 1;
   } else {
     alert('Sorry, try again');
+    gamesLost += 1;
   }
-};
-function flipCard(cardId) {
-  cardsInPlay.push(cards[cardId]);
-  console.log('User flipped '+ cards[cardId].rank + " of " + cards[cardId].suit);
-  checkForMatch();
+  updateScore();
 };
 
-  flipCard(0);
-  flipCard(0);
+function flipCard() {
+  // "this" refers to the actual "cardElement" clicked from "createBoard" function
+  var cardId = this.getAttribute('data-id');
+  cardsInPlay.push(cards[cardId]);
+  console.log('User flipped '+ cards[cardId].rank + " of " + cards[cardId].suit);
+  this.setAttribute('src', cards[cardId].cardImage);
+
+  switch(cardsInPlay.length) {
+    case 0:
+    // alert("Please flip two cards");
+    break;
+    case 1:
+    // alert("Please flip another card");
+    break;
+    case 2:
+    checkForMatch();
+    resetCards();
+    break;
+    default:
+    resetCards();
+      }
+};
+
+// Function below creates a new game board
+function createBoard() {
+  for (var i = 0; i < cards.length; i++){
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+
+  }
+};
+function resetCards() {
+  cardsInPlay.length = 0;
+  var parent = document.getElementById('game-board');
+  while(parent.firstChild) {
+    parent.firstChild.remove();
+  }
+  createBoard();
+};
+function updateScore() {
+  document.getElementById('games-won').innerHTML = gamesWon;
+  document.getElementById('games-lost').innerHTML = gamesLost;
+};
+function resetGame () {
+  gamesWon = 0;
+  gamesLost = 0;
+  updateScore();
+  resetCards();
+};
+var resetButton = document.getElementById('reset-button');
+resetButton.addEventListener('click', resetGame);
+createBoard();
